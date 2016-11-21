67301ef74a31ce0aee57fc472f488f2078972e66
alexKwonIsAwesome <khwalex@gmail.com>
2016-11-21 15:10:42 +0900

    rename file & update test: invalud input testing
    Along with the previous test, now it tests for invalid input which is
    not pure numbers. The same spy function works and 'expect' nothing to be
    happend

--- app/tests/components/CountdownForm.test.jsx
+++ app/tests/components/CountdownForm.test.jsx
@@ -1,6 +1,6 @@
-var expect = require('expert');
+var expect = require('expect');
 var React = require('react');
-var ReactDOM = require('reqct-dom');
+var ReactDOM = require('react-dom');
 var TestUtils = require('react-addons-test-utils');
 var $ = require('jquery');
 
@@ -20,5 +20,16 @@ describe('CountdownForm', () => {
     TestUtils.Simulate.submit($el.find('form')[0]);
 
     expect(spy).toHaveBeenCalledWith(109);
+  });
+
+  it('should not call onSetCountdown if invalid seconds entered', () => {
+    var spy = expect.createSpy()
+    var countdownForm = TestUtils.renderIntoDocument(<CountdownForm onSetCountdown={spy}/>);
+    var $el = $(ReactDOM.findDOMNode(countdownForm))
+
+    countdownForm.refs.seconds.value='testabc';
+    TestUtils.Simulate.submit($el.find('form')[0]);
+
+    expect(spy).toNotHaveBeenCalled();
   });
 });
